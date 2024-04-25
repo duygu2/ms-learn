5b28147aea7973727fb443b24db1c6ea4e7627eb
duygu2 <duyguorhan017@gmail.com>
2024-04-25 22:19:22 +0300

    added communication and management

---
+++ orderservice/src/main/java/com/example/orderservice/clients/ProductServiceClient.java
@@ -0,0 +1,12 @@
+package com.example.orderservice.clients;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name = "productservice")
+public interface ProductServiceClient {
+    @GetMapping("/api/v1/products")
+    int getStockByProductId(@RequestParam int productId);
+
+}
--- orderservice/src/main/java/com/example/orderservice/controllers/OrdersController.java
+++ orderservice/src/main/java/com/example/orderservice/controllers/OrdersController.java
@@ -1,18 +1,21 @@
 package com.example.orderservice.controllers;
 
+import com.example.orderservice.clients.ProductServiceClient;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @RestController
-@RequestMapping("/orders")
+@RequestMapping("/api/v1/orders")
 @RequiredArgsConstructor
 public class OrdersController {
-    private final WebClient.Builder webClientBuilder;
+   // private final WebClient.Builder webClientBuilder;
+
+    private final ProductServiceClient productServiceClient;
     @PostMapping
     public String addOrder(@RequestParam int productId){
 
-        var result=webClientBuilder
+       /* var result=webClientBuilder
                 .build()
                 .get()
                 .uri("http://localhost:8080/products?productId="+productId)
@@ -24,6 +27,16 @@ public class OrdersController {
             throw new RuntimeException("ürün stokta yok");
         }
         return "sipariş eklendi";
-    }
+    */
+        int stockResult= productServiceClient.getStockByProductId(productId);
 
+        System.out.println("Ürün servisinden gelen cevap: " + stockResult);
+
+
+        if(stockResult<0){
+            throw new RuntimeException("ürün stokta yok");
+        }
+        return "sipariş eklendi";
+
+}
 }
